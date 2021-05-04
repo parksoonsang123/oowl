686c3541b25f3a3a1d771f525da28e3180752bd9
Anchangmin <69341506+Anchangmin@users.noreply.github.com>
2021-05-04 15:24:46 +0900

    gallery

--- app/src/main/java/com/ooowl/oowl/MyGalleryFragment.java
+++ app/src/main/java/com/ooowl/oowl/MyGalleryFragment.java
@@ -2,18 +2,85 @@ package com.ooowl.oowl;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class MyGalleryFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    String userid;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_gallery, container, false);
+
+        final View view = inflater.inflate(R.layout.fragment_my_gallery, container, false);
+
+        recyclerView = view.findViewById(R.id.mg_recyclerview);
+        //recyclerView.setHasFixedSize(true);
+        layoutManager = new GridLayoutManager(view.getContext(),3,GridLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>();
+
+        userid = mAuth.getUid();
+
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Post");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                arrayList.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    PostItem postItem = snapshot.getValue(PostItem.class);
+                    if(postItem.getUserid().equals(userid)){
+                        for(int i=0;i<postItem.getImageurilist().size();i++){
+                            arrayList.add(postItem.getImageurilist().get(i));
+                        }
+                    }
+                }
+
+                adapter = new GalleryAdapter(arrayList, view.getContext());
+                recyclerView.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+
+        return view;
     }
+
+    /*public void store(View view) {
+    }
+
+    public void follwer(View view) {
+    }*/
+
 }
