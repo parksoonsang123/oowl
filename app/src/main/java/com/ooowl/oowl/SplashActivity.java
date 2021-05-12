96dbbd82f3b2bfee84fe67a6092c8f246d76da5a
user <pss5594@naver.com>
2021-05-12 11:55:52 +0900

    pss

---
+++ app/src/main/java/com/ooowl/oowl/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.ooowl.oowl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        try {
+            Thread.sleep(4000); //대기 초 설정
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            finish();
+        } catch (Exception e) {
+            Log.e("Error", "SplashActivity ERROR", e);//에러 로그
+        }
+
+    }
+}
