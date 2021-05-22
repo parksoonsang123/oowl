9dad0af3b086ec8431a27d26e872213153722ebf
Anchangmin <69341506+Anchangmin@users.noreply.github.com>
2021-05-22 17:28:51 +0900

    gallery

--- app/src/main/java/com/ooowl/oowl/FollowAdapter.java
+++ app/src/main/java/com/ooowl/oowl/FollowAdapter.java
@@ -2,6 +2,7 @@ package com.ooowl.oowl;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.auth.EmailAuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,6 +28,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class FollowAdapter extends RecyclerView.Adapter<FollowAdapter.ViewHolder>{
     private ArrayList<String> arrayList;
     private Context context;
+
 
     public FollowAdapter(ArrayList<String> arrayList, Context context) {
         this.arrayList = arrayList;
@@ -76,12 +80,31 @@ public class FollowAdapter extends RecyclerView.Adapter<FollowAdapter.ViewHolder
         TextView nickname;
         TextView follow_cnt;
         CircleImageView profile;
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        String userid = mAuth.getCurrentUser().getUid();
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             this.nickname = itemView.findViewById(R.id.f_nick);
             this.follow_cnt = itemView.findViewById(R.id.num_yourfer);
             this.profile = itemView.findViewById(R.id.f_image);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    final int pos = getAdapterPosition();
+                    Intent intent1;
+                    if(!arrayList.get(pos).equals(userid)) {
+                        intent1 = new Intent(view.getContext(), YourGallery.class);
+                        intent1.putExtra("your_id",arrayList.get(pos));
+                        view.getContext().startActivity(intent1);
+                    }
+                    else{   //마이 갤러리 프래그먼트로 이동
+
+                    }
+
+                }
+            });
         }
     }
 }
